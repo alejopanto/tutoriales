d06a3e66365498a95efc06b0931e10c1dd4441ad
alejopanto <aletop2@hotmail.com>
2020-08-19 22:04:12 -0500

    Curso Vue Adbance

---
+++ Platzi/VueJS/Advance Curse/platzi-music/src/App.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  #app
+    input(v-model="name")
+    button(v-on:click="format") format
+    p {{formatName}}
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      name: '',
+      formatName: ''
+    }
+  },
+  methods: {
+    format(){
+      this.formatName = this.name.charAt(0).toUpperCase() + this.name.slice(1)
+    }
+  },
+  
+}
+</script>
+
+<style lang="scss">
+  @import './scss/main.scss'
+</style>
