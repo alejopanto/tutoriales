0336c696e46a1f60622fbac96849448e1a734f39
alejopanto <aletop2@hotmail.com>
2020-08-20 16:10:01 -0500

    Components

--- Platzi/VueJS/Advance Curse/platzi-music/src/App.vue
+++ Platzi/VueJS/Advance Curse/platzi-music/src/App.vue
@@ -1,28 +1,75 @@
 <template lang="pug">
   #app
-    input(v-model="name")
-    button(v-on:click="format") format
-    p {{formatName}}
+    pm-header
+    
+    section.section
+      nav.nav.has-shadow  
+        .container
+          input.input.is-large(type="text", placeholder="Buscar Caciones", v-model="searchQuery")
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
+          p
+            small {{searhMessage}}
+
+      .container
+        .columns
+          .column(v-for="t in tracks") {{t.name}} {{t.artist}}
+
+    pm-footer
+
 </template>
 
 <script>
+
+import trackService from './services/track.js'
+import PmFooter from './components/layout/Footer.vue'
+import PmHeader from './components/layout/Header.vue'
+
+const tracks = [
+  {name: "muchacha", artist: "paquito"},
+  {name: "despacito", artist: "julion"},
+  {name: "gasolina", artist: "daddy"},
+]
+
 export default {
   name: 'app',
+
+  components: { PmFooter, PmHeader },
+
   data () {
     return {
-      name: '',
-      formatName: ''
+      searchQuery: '',
+      tracks: []
     }
   },
-  methods: {
-    format(){
-      this.formatName = this.name.charAt(0).toUpperCase() + this.name.slice(1)
+
+  computed: {
+    searhMessage(){
+      return `Total encontrados: ${this.tracks.length}`
     }
+  },
+
+  methods: {
+    search(){
+      if(!this.searchQuery) {return}
+
+      this.tracks = tracks
+
+
+      // trackService.search(this.searchQuery)
+      //   .then(res => {
+      //     this.tracks = res.tracks.items
+      //   })
+    },
   },
   
 }
 </script>
 
 <style lang="scss">
-  @import './scss/main.scss'
+  @import './scss/main.scss';
+
+  .results {
+    margin-top: 50px;
+  }
 </style>
---
+++ Platzi/VueJS/Advance Curse/platzi-music/src/components/layout/Footer.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+    footer.footer
+        .container
+            .container.has-text-centered
+                p echo con amor
+</template>
