c18485c668794fe1e5a4bcf61ba785d7320529a9
alejopanto <aletop2@hotmail.com>
2020-11-08 14:58:13 -0500

    Vue platzi Platzimusic

--- Platzi/VueJS/Advance Curse/platzi-music/src/App.vue
+++ Platzi/VueJS/Advance Curse/platzi-music/src/App.vue
@@ -2,18 +2,26 @@
   #app
     pm-header
     
+    pm-notification(v-show="showNotification")
+      p(slot="body") Superaste mas de 10 carateres
+
+    pm-loader(v-show="isLoading")
+    //- (v-show="!isLoading") en el section
     section.section
-      nav.nav.has-shadow  
+      nav.nav
         .container
-          input.input.is-large(type="text", placeholder="Buscar Caciones", v-model="searchQuery")
+          input.input.is-large(type="text", placeholder="Buscar Caciones (Alarma a los 10 caracteres)", v-model="searchQuery")
           a.button.is-info.is-large(@click="search") Buscar
           a.button.is-danger.is-large &times;
+
+        .container
           p
             small {{searhMessage}}
 
-      .container
-        .columns
-          .column(v-for="t in tracks") {{t.name}} {{t.artist}}
+      .container.results
+        .columns.is-multiline
+          .column.is-one-quarter(v-for="t in tracks")
+            pm-track(:class="{ 'is-active' : t.id == selectedTrack }", :track="t", @select="setSelectedTrack")
 
     pm-footer
 
@@ -21,25 +29,36 @@
 
 <script>
 
-import trackService from './services/track.js'
-import PmFooter from './components/layout/Footer.vue'
-import PmHeader from './components/layout/Header.vue'
+import trackService from '@/services/track.js'
+
+import PmFooter from '@/components/layout/Footer.vue'
+import PmHeader from '@/components/layout/Header.vue'
+
+import PmTrack from '@/components/Track.vue'
+
+import PmLoader from '@/components/shared/Loader.vue'
+import PmNotification from '@/components/shared/Notification.vue'
 
 const tracks = [
-  {name: "muchacha", artist: "paquito"},
-  {name: "despacito", artist: "julion"},
-  {name: "gasolina", artist: "daddy"},
+  {id: 1, name: "muchacha", artist: "paquito", img:"https://los40es00.epimg.net/los40/imagenes/2017/11/28/album/1511885438_220718_1512034270_portada_normal.jpg", duration: 228626},
+  {id: 2, name: "despacito", artist: "julion", img:"https://los40.com/los40/imagenes/2017/11/28/album/1511885438_220718_1512040978_album_normal.jpg", duration: 124587},
+  {id: 3, name: "gasolina", artist: "daddy", img:"https://los40.com/los40/imagenes/2016/10/20/album/1476974044_548008_1477034844_album_normal.jpg", duration: 968574},
 ]
 
 export default {
   name: 'app',
 
-  components: { PmFooter, PmHeader },
+  components: { PmFooter, PmHeader, PmTrack, PmLoader, PmNotification },
 
   data () {
     return {
       searchQuery: '',
-      tracks: []
+      tracks: [],
+
+      isLoading: false,
+      showNotification: false,
+
+      selectedTrack: ''
     }
   },
 
@@ -49,18 +68,38 @@ export default {
     }
   },
 
+  watch: {
+    showNotification(){
+      if(this.showNotification){
+        setTimeout( () => {
+          this.showNotification = false
+        }, 3000)
+      }
+    }
+  },
+
   methods: {
+
     search(){
+
       if(!this.searchQuery) {return}
+      this.isLoading = true
+
+      this.showNotification = this.searchQuery.length > 10
 
       this.tracks = tracks
-
 
       // trackService.search(this.searchQuery)
       //   .then(res => {
       //     this.tracks = res.tracks.items
+      //     this.isLoading = false
       //   })
     },
+
+    setSelectedTrack(id){
+      this.selectedTrack = id
+    }
+
   },
   
 }
@@ -71,5 +110,9 @@ export default {
 
   .results {
     margin-top: 50px;
+  }
+
+  .is-active {
+    border: 3px #23d160 solid;
   }
 </style>
---
+++ Platzi/VueJS/Advance Curse/platzi-music/src/components/Track.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+    .card   
+        .card-image
+            figure.image.is-1by1
+                img(:src="track.img")
+        .card-content
+            .media
+                .media-left
+                    figure.image.is-48x48
+                        img(:src="track.img")
+                .media-content
+                    p.title.is-6    
+                        strong {{track.name}}
+                    p.subtitle.is-6 {{track.artist}}
+            
+            .content
+                small {{track.duration}}
+                nav.level
+                    .level.left
+                        a.level-item
+                            span.icon.is-small(@click="selectTrack") PLAY
+</template>
+
+<script>
+export default {
+    props: {
+        track: { type: Object, required: true}
+    },
+    methods: {
+        selectTrack(){
+            this.$emit('select', this.track.id)
+        }
+    },
+}
+</script>
